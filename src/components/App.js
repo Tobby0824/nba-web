07c0f00736f3fc44f36addbe83005f529faff4df
Tobby0824 <taoxuan0824@gmail.com>
2018-04-07 17:33:15 -0400

    reorg files

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import logo from '../assets/images/logo.svg';
 import '../styles/App.css';
+import {TopNavBar} from "./TopNavBar"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+       <TopNavBar/>
       </div>
     );
   }
