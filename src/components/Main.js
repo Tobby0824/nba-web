50cdd6f1eb25d61c1411edbc4f792e21adbb399b
Tobby0824 <taoxuan0824@gmail.com>
2018-04-07 19:44:55 -0400

    show one player final

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,7 +21,7 @@ export class Main extends React.Component {
     render() {
         return(
             <div className="main">
-                <Profile/>
+                <Profile playerInfo={this.state.playerInfo}/>
                 <ShotChart playerId={this.state.playerId}/>
             </div>
         );
