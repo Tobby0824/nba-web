10d153a4c03394dbe08aed13f06b971c559f7731
Tobby0824 <taoxuan0824@gmail.com>
2018-04-12 23:25:37 -0400

    count slider

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,14 +5,17 @@ import {DataViewContainer} from "./DataViewContainer"
 
 export class Main extends React.Component {
     state = {
-        playerId: nba.findPlayer('Stephen Curry').playerId,
+        //playerId: nba.findPlayer('Stephen Curry').playerId,
         //Season: '2013-14',
         //playerId: 1503,
-        playerInfo: {}
+        playerInfo: {
+            playerId: nba.findPlayer('Stephen Curry').playerId,
+            teamAbbreviation: 'GSW',
+        }
     }
 
     componentDidMount() {
-        nba.stats.playerInfo({ PlayerID: this.state.playerId}).then((info) => {
+        nba.stats.playerInfo({ PlayerID: nba.findPlayer('Stephen Curry').playerId}).then((info) => {
             // merge two objects of info
             const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
             console.log(playerInfo);
@@ -25,7 +28,7 @@ export class Main extends React.Component {
         return(
             <div className="main">
                 <Profile playerInfo={this.state.playerInfo}/>
-                <DataViewContainer playerId={this.state.playerId}/>
+                <DataViewContainer playerId={this.state.playerInfo.playerId}/>
             </div>
         );
     }
